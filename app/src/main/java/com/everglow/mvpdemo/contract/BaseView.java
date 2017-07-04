5e544b56ed54ef8dcddf37c431841e02fcc78f3c
YougaKing <yougakingwu@gmail.com>
2017-07-04 11:22:38 +0800

    add unSubscribe

--- app/src/main/java/com/everglow/mvpdemo/contract/BaseView.java
+++ app/src/main/java/com/everglow/mvpdemo/contract/BaseView.java
@@ -5,5 +5,6 @@ package com.everglow.mvpdemo.contract;
  */
 
 public interface BaseView<T> {
+
     void setPresenter(T presenter);
 }
--- app/src/main/java/com/everglow/mvpdemo/presenter/LoginPresenter.java
+++ app/src/main/java/com/everglow/mvpdemo/presenter/LoginPresenter.java
@@ -1,4 +1,5 @@
 package com.everglow.mvpdemo.presenter;
+
 import com.everglow.mvpdemo.contract.UserContract;
 import com.everglow.mvpdemo.model.UserInfoBean;
 import com.everglow.mvpdemo.utils.Tools;
@@ -7,6 +8,8 @@ import com.zhy.http.okhttp.OkHttpUtils;
 import com.zhy.http.okhttp.callback.Callback;
 import com.zhy.http.okhttp.https.HttpsUtils;
 
+import java.util.Map;
+
 import okhttp3.Call;
 import okhttp3.Response;
 
@@ -14,24 +17,35 @@ import okhttp3.Response;
  * Created by EverGlow on 2017/7/3 16:22
  */
 
-public    class LoginPresenter  implements UserContract.Persenter  {
+public class LoginPresenter implements UserContract.Presenter {
 
     UserContract.LoginView view;
-    public LoginPresenter(UserContract.LoginView view) {
+    boolean mUnSubscribe;
 
+    public LoginPresenter(UserContract.LoginView view) {
         this.view = view;
         view.setPresenter(this);
     }
 
     @Override
-    public void Login() {
+    public void onSubscribe() {
+
+    }
+
+    @Override
+    public void unSubscribe() {
+        mUnSubscribe = true;
+    }
+
+    @Override
+    public void login(Map<String, String> map) {
         view.showLoading();
         OkHttpUtils.post()
                 .url("http://yl.cgsoft.net/index.php?g=cgapik&m=index&a=do_login")
-                .addParams("keynumber",view.getCompanyId())
-                .addParams("username",view.getUserName())
-                .addParams("token","43378e1b35ae7858e82eba2b27ddefd7")
-                .addParams("password", Tools.encryptPasswordMD5(view.getPassword()))
+                .addParams("keynumber", map.get("keynumber"))
+                .addParams("username", map.get("username"))
+                .addParams("token", "43378e1b35ae7858e82eba2b27ddefd7")
+                .addParams("password", Tools.encryptPasswordMD5(map.get("password")))
                 .build()
                 .execute(new Callback<UserInfoBean>() {
                     @Override
@@ -42,25 +56,20 @@ public    class LoginPresenter  implements UserContract.Persenter  {
                     @Override
                     public void onError(Call call, Exception e, int id) {
                         e.printStackTrace();
+                        if (mUnSubscribe) return;
                         view.hideLoading();
                         view.showFailedError();
                     }
 
                     @Override
                     public void onResponse(UserInfoBean response, int id) {
-                        if(response.getCode() ==1){
+                        if (mUnSubscribe) return;
+                        if (response.getCode() == 1) {
                             view.toMainActivity(response);
-                        }else{
-                            view.cleanEdit();
                         }
                         view.toastMessage(response);
                         view.hideLoading();
                     }
                 });
-    }
-
-    @Override
-    public void start() {
-            Login();
     }
 }
